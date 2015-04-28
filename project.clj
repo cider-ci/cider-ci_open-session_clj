5d534625f929949cb436ba90d04a898572450fc9
Thomas Schank <DrTom@schank.ch>
2015-04-28 19:52:17 +0200

    Set version to 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cider-ci/open-session "1.1.0-beta.3"
+(defproject cider-ci/open-session "1.1.0"
   :description "Platform and language agnostic encoding, encryption, signature and password handling."
   :url "https://github.com/cider-ci/cider-ci_open-session_clj"
   :license {:name "APGL"}
